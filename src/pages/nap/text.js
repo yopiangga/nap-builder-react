861ddb3134da50567d09f073f01d91988eb48411
yopiangga <yopigambyok@gmail.com>
2025-03-24 06:06:19 +0700

    setup: test

--- src/pages/nap/text.js
+++ src/pages/nap/text.js
@@ -5,6 +5,10 @@ export function TextComponent({ title, description, reference }) {
         <span className="text-slate-900 font-bold">{title}</span>
       </h4>
       <p className="mt-2 text-justify">{description}</p>
+      <p className="mt-2 text-justify italic text-sm">
+        <span className="text-slate-900 font-medium">Reference : </span>
+        {reference}
+      </p>
     </div>
   );
 }
